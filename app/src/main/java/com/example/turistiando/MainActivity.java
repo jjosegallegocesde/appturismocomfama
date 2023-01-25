b91c9b08e991d20ed0494617a2729177b24dba7a
205 <205@academicos.cesde.edu.co>
2023-01-24 20:41:33 -0500

    se crea menu de opciones

--- app/src/main/java/com/example/turistiando/MainActivity.java
+++ app/src/main/java/com/example/turistiando/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.turistiando;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //ATRIBUTOS
+
+    //METODOS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    //Cargar el menu xml creado previamente
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+    }
+
+
+    //DAR funcionamiento al menu
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //observamos a que item del menu le damos clic
+        int itemSeleccionado=item.getItemId();
+
+        switch(itemSeleccionado){
+
+            case(R.id.opcion1):
+                Toast.makeText(this, "seleccionaste opcion1", Toast.LENGTH_SHORT).show();
+                break;
+            case(R.id.opcion2):
+                Toast.makeText(this, "seleccionaste opcion2", Toast.LENGTH_SHORT).show();
+                break;
+            case(R.id.opcion3):
+                Toast.makeText(this, "seleccionaste opcion3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
