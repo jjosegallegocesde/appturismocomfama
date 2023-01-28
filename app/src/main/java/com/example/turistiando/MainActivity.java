f9df3ee7f05e7697b4043228b869880be711aa79
205 <205@academicos.cesde.edu.co>
2023-01-27 20:29:29 -0500

    ok

--- app/src/main/java/com/example/turistiando/MainActivity.java
+++ app/src/main/java/com/example/turistiando/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.turistiando;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar barra=getSupportActionBar();
+
+        //Cambiando el color del action bar
+        ColorDrawable colorBarra= new ColorDrawable(Color.parseColor("#2BAA61"));
+        barra.setBackgroundDrawable(colorBarra);
+
+    }
+
+
+    //metodo para cambiar el idioma de mi app
+    public void cambiarIdioma(String idioma){
+
+        //configurando el lenguaje del telefono
+        Locale lenguaje=new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configracion global en el telefono
+        Configuration configuracionTelefono=getResources().getConfiguration();
+        configuracionTelefono.locale=lenguaje;
+
+        //ejecutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
+
     }
 
 
@@ -39,10 +70,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "seleccionaste opcion1", Toast.LENGTH_SHORT).show();
                 break;
             case(R.id.opcion2):
-                Toast.makeText(this, "seleccionaste opcion2", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("en");
+                Intent intent1=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent1);
                 break;
             case(R.id.opcion3):
-                Toast.makeText(this, "seleccionaste opcion3", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("es");
+                Intent intent2=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent2m                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      );
                 break;
         }
 
