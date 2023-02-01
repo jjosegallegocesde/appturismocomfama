360c0069adce80e9f45364d32aefb45b08ac0b4b
205 <205@academicos.cesde.edu.co>
2023-01-31 20:57:35 -0500

    ok

--- app/src/main/java/com/example/turistiando/MainActivity.java
+++ app/src/main/java/com/example/turistiando/MainActivity.java
@@ -1,21 +1,72 @@
 package com.example.turistiando;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     //ATRIBUTOS
+    ImageButton botonHoteles;
+    ImageButton botonRestaurante;
 
     //METODOS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar barra=getSupportActionBar();
+
+        //Cambiando el color del action bar
+        ColorDrawable colorBarra= new ColorDrawable(Color.parseColor("#2BAA61"));
+        barra.setBackgroundDrawable(colorBarra);
+
+        //asociando los botones a eventos de clic (escuchar clic ome)
+        botonHoteles=findViewById(R.id.iconohotel);
+        botonRestaurante=findViewById(R.id.iconorestaurante);
+
+        botonHoteles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentHoteles= new Intent(MainActivity.this,Hoteles.class);
+                startActivity(intentHoteles);
+
+            }
+        });
+
+
+
+    }
+
+
+    //metodo para cambiar el idioma de mi app
+    public void cambiarIdioma(String idioma){
+
+        //configurando el lenguaje del telefono
+        Locale lenguaje=new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configracion global en el telefono
+        Configuration configuracionTelefono=getResources().getConfiguration();
+        configuracionTelefono.locale=lenguaje;
+
+        //ejecutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
+
     }
 
 
@@ -39,10 +90,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "seleccionaste opcion1", Toast.LENGTH_SHORT).show();
                 break;
             case(R.id.opcion2):
-                Toast.makeText(this, "seleccionaste opcion2", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("en");
+                Intent intent1=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent1);
                 break;
             case(R.id.opcion3):
-                Toast.makeText(this, "seleccionaste opcion3", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("es");
+                Intent intent2=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent2);
                 break;
         }
 
